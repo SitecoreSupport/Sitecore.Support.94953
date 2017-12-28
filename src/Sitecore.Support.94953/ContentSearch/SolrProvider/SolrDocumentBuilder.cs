df61c5b643ff9647f2449fd8358c9604a3bf8b06
Vladyslav Molibozhenko <vlm@sitecore.net>
2017-12-28 11:33:49 +0200

    initial code commit

---
+++ src/Sitecore.Support.94953/ContentSearch/SolrProvider/SolrDocumentBuilder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+using Sitecore.ContentSearch.Diagnostics;
+using Sitecore.ContentSearch.Utilities;
+using Sitecore.Data.Items;
+using Sitecore.Data.LanguageFallback;
+using Sitecore.Data.Managers;
+using Sitecore.Diagnostics;
+using Sitecore.ContentSearch;
+
+namespace Sitecore.Support.ContentSearch.SolrProvider
+{
+  public class SolrDocumentBuilder : Sitecore.ContentSearch.SolrProvider.SolrDocumentBuilder
+  {
+
+    public SolrDocumentBuilder(IIndexable indexable, IProviderUpdateContext context) : base(indexable, context)
+    {
+
+    }
+
+
+    protected override void AddItemFields()
+    {
+      try
+      {
+        VerboseLogging.CrawlingLogDebug(() => "AddItemFields start");
+
+        if (this.Options.IndexAllFields)
+        {
+          this.Indexable.LoadAllFields();
+        }
+
+        if (IsParallel)
+        {
+          var exceptions = new ConcurrentQueue<Exception>();
+          Parallel.ForEach(this.Indexable.Fields, this.ParallelOptions, f =>
+          {
+            try
+            {
+              this.CheckAndAddField(this.Indexable, f);
+            }
+            catch (Exception ex)
+            {
+              exceptions.Enqueue(ex);
+            }
+          });
+
+          if (exceptions.Count > 0)
+          {
+            throw new AggregateException(exceptions);
+          }
+        }
+        else
+        {
+          foreach (var field in this.Indexable.Fields)
+          {
+            this.CheckAndAddField(this.Indexable, field);
+          }
+        }
+      }
+      finally
+      {
+        VerboseLogging.CrawlingLogDebug(() => "AddItemFields End");
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.94953/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.94953")]
+[assembly: AssemblyProduct("Sitecore.Support.94953")]
+[assembly: ComVisible(false)]
