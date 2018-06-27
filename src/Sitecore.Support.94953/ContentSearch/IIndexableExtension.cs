59231fe403b00856428a85aca23ac67741e0cb72
dk\ivd <ivd@sitecore.net>
2018-06-27 15:51:52 +0800

    Introduce IIndexableExtension

---
+++ src/Sitecore.Support.94953/ContentSearch/IIndexableExtension.cs
@@ -0,0 +1,70 @@
+﻿
+namespace Sitecore.Support.ContentSearch
+{
+  using Data;
+  using Data.Managers;
+  using Diagnostics;
+  using Sitecore.ContentSearch;
+  using Sitecore.ContentSearch.Diagnostics;
+  using StringExtensions;
+
+  public static class IIndexableExtension
+  {
+    public static IIndexableDataField GetFieldByKey(this IIndexable indexable, object fieldKey)
+    {
+      if (indexable == null || fieldKey == null)
+      {
+        return null;
+      }
+
+      // Is Sitecore Item Related?
+      var itemIndexable = indexable as SitecoreIndexableItem;
+
+      if (itemIndexable == null)
+      {
+        // No, default logic:
+        return indexable.GetFieldById(fieldKey);
+      }
+
+      ID fieldId;
+
+      if (ID.TryParse(fieldKey, out fieldId))
+      {
+        if (itemIndexable.OwnsField(fieldId))
+        {
+          return itemIndexable.GetFieldById(fieldId);
+        }
+
+        return null;
+      }
+
+      var fieldName = fieldKey as string;
+
+      if (fieldName != null)
+      {
+        return indexable.GetFieldByName(fieldName);
+      }
+
+      return null;
+    }
+
+    private static bool OwnsField([NotNull] this SitecoreIndexableItem indexable, [NotNull] ID fieldId)
+    {
+      Assert.ArgumentNotNull(indexable, nameof(indexable));
+      Assert.ArgumentNotNull(fieldId, nameof(fieldId));
+
+      var item = indexable.Item;
+
+      if (item == null)
+      {
+        CrawlingLog.Log.Warn("SUPPORT SitecoreIndexableItem '{0}' doesn't contain inner Item."
+          .FormatWith(indexable.UniqueId)
+        );
+
+        return false;
+      }
+
+      return TemplateManager.IsFieldPartOfTemplate(fieldId, item);
+    }
+  }
+}
