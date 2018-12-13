16bd84773d329dd2947f84766376f81c88d3274c
dk\ivd <ivd@sitecore.net>
2018-12-13 13:51:43 +0800

    Switch to 8.2.6

--- src/Sitecore.Support.94953/ContentSearch/SolrProvider/SolrDocumentBuilder.cs
+++ src/Sitecore.Support.94953/ContentSearch/SolrProvider/SolrDocumentBuilder.cs
@@ -1,13 +1,9 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Concurrent;
-using System.Threading.Tasks;
 
 using Sitecore.ContentSearch.Diagnostics;
-using Sitecore.ContentSearch.Utilities;
-using Sitecore.Data.Items;
 using Sitecore.Data.LanguageFallback;
-using Sitecore.Data.Managers;
-using Sitecore.Diagnostics;
 using Sitecore.ContentSearch;
 
 namespace Sitecore.Support.ContentSearch.SolrProvider
@@ -20,8 +16,7 @@ namespace Sitecore.Support.ContentSearch.SolrProvider
 
     }
 
-
-    protected override void AddItemFields()
+    public override void AddItemFields()
     {
       try
       {
@@ -35,7 +30,8 @@ namespace Sitecore.Support.ContentSearch.SolrProvider
         if (IsParallel)
         {
           var exceptions = new ConcurrentQueue<Exception>();
-          Parallel.ForEach(this.Indexable.Fields, this.ParallelOptions, f =>
+
+          this.ParallelForeachProxy.ForEach(this.Indexable.Fields, this.ParallelOptions, f =>
           {
             try
             {
@@ -63,6 +59,67 @@ namespace Sitecore.Support.ContentSearch.SolrProvider
       finally
       {
         VerboseLogging.CrawlingLogDebug(() => "AddItemFields End");
+      }
+    }
+
+    private void CheckAndAddField(IIndexable indexable, IIndexableDataField field)
+    {
+      var fieldKey = field.Name;
+
+      if (this.IsTemplate && this.Options.HasExcludedTemplateFields)
+      {
+        if (this.Options.ExcludedTemplateFields.Contains(fieldKey) || this.Options.ExcludedTemplateFields.Contains(field.Id.ToString()))
+        {
+          VerboseLogging.CrawlingLogDebug(() => string.Format("Skipping field id:{0}, name:{1}, typeKey:{2} - Field was excluded.", field.Id, field.Name, field.TypeKey));
+          return;
+        }
+      }
+
+      if (this.IsMedia && this.Options.HasExcludedMediaFields)
+      {
+        if (this.Options.ExcludedMediaFields.Contains(field.Name))
+        {
+          VerboseLogging.CrawlingLogDebug(() => string.Format("Skipping field id:{0}, name:{1}, typeKey:{2} - Media field was excluded.", field.Id, field.Name, field.TypeKey));
+          return;
+        }
+      }
+
+      if (this.Options.ExcludedFields.Contains(field.Id.ToString()) || this.Options.ExcludedFields.Contains(fieldKey))
+      {
+        VerboseLogging.CrawlingLogDebug(() => string.Format("Skipping field id:{0}, name:{1}, typeKey:{2} - Field was excluded.", field.Id, field.Name, field.TypeKey));
+        return;
+      }
+
+      try
+      {
+        if (this.Options.IndexAllFields)
+        {
+          using (new LanguageFallbackFieldSwitcher(this.Index.EnableFieldLanguageFallback))
+          {
+            this.AddField(field);
+          }
+        }
+        else
+        {
+          if (this.Options.IncludedFields.Contains(fieldKey) || this.Options.IncludedFields.Contains(field.Id.ToString()))
+          {
+            using (new LanguageFallbackFieldSwitcher(this.Index.EnableFieldLanguageFallback))
+            {
+              this.AddField(field);
+            }
+          }
+          else
+          {
+            VerboseLogging.CrawlingLogDebug(() => string.Format("Skipping field id:{0}, name:{1}, typeKey:{2} - Field was not included.", field.Id, field.Name, field.TypeKey));
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+        if (!this.Settings.StopOnCrawlFieldError())
+          CrawlingLog.Log.Fatal(string.Format("Could not add field {1} : {2} for indexable {0}", indexable.UniqueId, field.Id, field.Name), ex);
+        else
+          throw;
       }
     }
   }
